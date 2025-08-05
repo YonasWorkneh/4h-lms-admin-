1d810d878de00de7d07a53af0aefca3b19850f7e
YonasWorkneh <yonasdegefu158@gmail.com>
2025-08-05 03:44:08 +0300

    rm unused cpts

--- app/page.tsx
+++ app/page.tsx
@@ -55,17 +55,13 @@ export default function Dashboard() {
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mb-8">
         {/* Left Column - 2/3 width */}
         <div className="lg:col-span-2 space-y-8">
-          {/* <AnalyticsChart /> */}
           <UpcomingClasses />
-          {/* <CourseList /> */}
           <CourseProgress />
         </div>
 
         {/* Right Column - 1/3 width */}
         <div className="space-y-8">
           <StudentList />
-          {/* <TimeTracker /> */}
-          {/* <MobileApp /> */}
         </div>
       </div>
     </div>
